4ec705609781bdb483143afffea30711533802d2
DanisStepanov <144427604+DanisStepanov@users.noreply.github.com>
2025-07-22 15:45:43 +0300

    исправление ошибок

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     
     
     def load_metrics():
-        metrics_df = pd.read_csv('metrics.csv')
+        metrics_df = pd.read_csv('metrics_okr.csv')
         return metrics_df.set_index('metric')['value'].to_dict()
 
 
@@ -31,9 +31,9 @@ def main():
     model = load_model()
 
     priznak = [
-    'sqft_living_scaled', 'bedrooms', 'bathrooms', 'sqft_lot_scaled', 
-    'grade_scaled', 'view_scaled', 'floors','sqft_above_scaled',
-    'waterfront', 'yr_built','sqft_above','condition_scaled', 'sqft_basement', 'yr_renovated','lat','long',]
+    'sqft_living_scaled', 'bedrooms', 'bathrooms', 
+    'grade_scaled', 'view_scaled', 'floors',
+    'waterfront', 'yr_built','sqft_above_scaled','condition_scaled', 'sqft_basement_scaled', 'yr_renovated','lat','long','sqft_lot_scaled']
 
     X = df[priznak]
     y = df['price']
@@ -46,24 +46,139 @@ def main():
 
 
     st.title("Степанов Денис Алексеевич_2023-ФГиИБ-ПИ-1б_20 Вариант_Рынок недвижемости")
-    st.write("В своей работе я рассматривал рынок недвижимости до 2024 года.id — Уникальный идентификационный номер, присвоенный каждому дому в наборе данных." \
-    " date — Дата добавления дома в набор данных в формате ГГГГ-ММ-ДД."\
-    " price — Цена дома в долларах США. bedrooms — Количество спален в доме, в наборе данных встречаются дома с 0 до 33 спален."\
-    " bathrooms — Количество ванных комнат в доме, варьируется от 0 до 8. sqft_living — Площадь жилой зоны в квадратных футах. "\
-    "sqft_lot — Общая площадь участка в квадратных футах. floors — Количество этажей в доме."\
-    " waterfront — Индикатор расположения дома у воды (озеро или пляж): 0 — нет, 1 — да." \
-    " view — Оценка вида на город, озеро или пляж из дома, от 0 до 5. condition — Общая оценка состояния дома, от 1 до 5." \
-    " grade — Общая оценка качества дома, от 1 до 12. sqft_above — Площадь дома над уровнем земли в квадратных футах." \
-    " sqft_basement — Площадь подвала дома (ниже уровня земли) в квадратных футах. yr_built — Год постройки дома." \
-    " yr_renovated — Год проведения ремонта или реконструкции дома. zipcode — Почтовый индекс (5 цифр), в котором расположен дом." \
-    " lat — Географическая широта расположения дома. long — Географическая долгота расположения дома. " \
-    "sqft_living15 — Средняя площадь жилой зоны 15 ближайших домов в квадратных футах." \
-    " sqft_lot15 — Средняя площадь участка 15 ближайших домов в квадратных футах.")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("""
+        В своей работе я рассматривал рынок недвижимости USA до 2024 года.
+
+        **id** — Уникальный идентификационный номер, присвоенный каждому дому в наборе данных.  
+        **date** — Дата добавления дома в набор данных в формате ГГГГ-ММ-ДД.  
+        **price** — Цена дома в долларах США.  
+        **bedrooms** — Количество спален в доме, в наборе данных встречаются дома с 0 до 33 спален.  
+        **bathrooms** — Количество ванных комнат в доме, варьируется от 0 до 8.  
+        **sqft_living** — Площадь жилой зоны в квадратных футах.  
+        **sqft_lot** — Общая площадь участка в квадратных футах.  
+        **floors** — Количество этажей в доме.  
+        **waterfront** — Индикатор расположения дома у воды (озеро или пляж): 0 — нет, 1 — да.  
+        **view** — Оценка вида на город, озеро или пляж из дома, от 0 до 5.  """)
+    with col2:
+        st.markdown("""
+                    
+
+        **condition** — Общая оценка состояния дома, от 1 до 5.  
+        **grade** — Общая оценка качества дома, от 1 до 12.  
+        **sqft_above** — Площадь дома над уровнем земли в квадратных футах.  
+        **sqft_basement** — Площадь подвала дома (ниже уровня земли) в квадратных футах.  
+        **yr_built** — Год постройки дома.  
+        **yr_renovated** — Год проведения ремонта или реконструкции дома.  
+        **zipcode** — Почтовый индекс (5 цифр), в котором расположен дом.  
+        **lat** — Географическая широта расположения дома.  
+        **long** — Географическая долгота расположения дома.  
+        **sqft_living15** — Средняя площадь жилой зоны 15 ближайших домов в квадратных футах.  
+        **sqft_lot15** — Средняя площадь участка 15 ближайших домов в квадратных футах.
+        """)
+
     
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["SHAP анализ и результаты обучения модели","Топ прогнозов по ошибке и распределение цен","Графики зависимостей", "Метрики модели", "Исходные данные"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Графики зависимостей","SHAP анализ и результаты обучения модели","Топ прогнозов по ошибке и распределение цен", "Метрики модели", "Исходные данные"])
     
     with tab1:
-        col1, col2, col3 = st.columns(3)
+            st.subheader("Зависимость цены от характеристик дома")
+                
+            col1, col2 = st.columns(2)
+                
+            with col1:
+                chart1 = alt.Chart(df).mark_boxplot(extent='min-max').encode(
+                x=alt.X('bedrooms:O', title='Количество спален', axis=alt.Axis(labelAngle=0)),
+                y=alt.Y('price:Q', title='Цена', scale=alt.Scale(zero=False))
+            ).properties(
+                width=400,
+                height=300,
+                title="Распределение цен по количеству спален"
+            )
+            
+            mean_line = alt.Chart(df).mark_point(filled=True, size=50, color='red').encode(
+                x='bedrooms:O',
+                y='mean(price):Q'
+            )
+            
+            st.altair_chart(chart1 + mean_line, use_container_width=True)
+            st.write('''
+            Наибольший разброс цен у домов с 3-4 спальнями.
+            Средняя цена растет от 1 до 4 спален, затем не изменяется существенно, потом резкий рост на 9 спален. После 9 спален средняя цена падает вниз.
+            Красные точки показывают средние значения
+            ''')
+
+            chart5 = alt.Chart(df).mark_circle(size=8, opacity=0.6).encode(
+                x=alt.X('lat:Q', title='Широта', bin=alt.Bin(maxbins=30)),
+                y=alt.Y('price:Q', title='Цена', scale=alt.Scale(domain=[0, 2000000])),
+                color=alt.Color('mean(price):Q', scale=alt.Scale(scheme='goldorange')),
+                size='count()',
+                tooltip=['lat', 'mean(price)', 'count()']
+            ).properties(
+                width=400,
+                height=300,
+                title="Географическое распределение цен (широта)"
+            ).interactive()
+            
+            st.altair_chart(chart5, use_container_width=True)
+            st.write('''
+            Самые дорогие дома находятся 47.5-47.7 широты. Чем краснее цвет круга, тем дороже дом.
+            ''')
+
+            with col2:
+                chart2 = alt.Chart(df).mark_circle(size=40, opacity=0.5).encode(
+                x=alt.X('bathrooms:Q', 
+                    title='Количество ванных комнат',
+                    scale=alt.Scale(nice=False)),
+                y=alt.Y('price:Q', title='Цена'),
+                tooltip=['bathrooms', 'price']
+            ).properties(
+                width=400,
+                height=300,
+                title="Зависимость цены от количества ванных комнат"
+            )
+            
+            trend = chart2.transform_regression(
+                'bathrooms', 'price'
+            ).mark_line(color='red')
+            
+            st.altair_chart(chart2 + trend, use_container_width=True)
+            st.write('''
+            Красная линия тренда, уверенно движется вверх, то есть чем больше ванных комнат, тем выше цена.
+            ''')
+
+            chart6 = alt.Chart(df).mark_rect().encode(
+                x=alt.X('long:Q', title='Долгота', bin=alt.Bin(maxbins=30)),
+                y=alt.Y('price:Q', title='Цена', bin=alt.Bin(maxbins=20, extent=[0, 2000000])),
+                color=alt.Color('mean(price):Q', 
+                            scale=alt.Scale(scheme='greenblue'),
+                            legend=alt.Legend(title="Средняя цена"))
+            ).properties(
+                width=400,
+                height=300,
+                title="Распределение цен по долготе"
+            )
+            
+            st.altair_chart(chart6, use_container_width=True)
+            st.write('''
+            Самые дорогие дома находтся на долготе -122.45 до -122. Чем синее цветы тем дороже, чем белее тем дешевле дома.
+            ''')
+            # chart4 = alt.Chart(df).mark_bar().encode(
+            #     x=alt.X('floors:O', title='Количество этажей'),
+            #     y=alt.Y('mean(price):Q', title='Цена'),
+            #     tooltip=['floors', 'price']
+            # ).properties(
+            #     width=400,
+            #     height=300,
+            #     title="Цена по количеству этажей"
+            # )
+            # st.altair_chart(chart4, use_container_width=True)
+            # st.write('Связь между площадью участка  и ценой. Резкий рост цены , затем слабая зависимость, имеются отдельные участки с большой площадью, скорее всего особняки, но из-за маленькой цены, видимо плохого качества.Площадь была стандартизирована')
+
+
+    with tab2:
+        col1, col2 = st.columns(2)
         
         with col1:
             st.subheader("Важность признаков")
@@ -82,22 +197,9 @@ def main():
             plt.tight_layout()
             st.pyplot(fig2)
             st.write('График показывает, как каждый признак (по оси Y) влияет на прогноз: Красные точки — высокие значения признака увеличивают цену. Синие точки — низкие значения уменьшают цену.')
-        with col3:
-            st.subheader("Сравнение фактических и предсказанных цен")
-            fig, ax = plt.subplots(figsize=(10, 6))
-            ax.scatter(y_test, y_pred, alpha=0.5, label="Прогнозы")
-            lims = [
-            np.min([y_test.min(), y_pred.min()]),
-            np.max([y_test.max(), y_pred.max()]),
-            ]
-            ax.plot(lims, lims, 'r--', label="Идеальная линия")
-            ax.set_xlabel("Фактическая цена ($)", fontsize=12)
-            ax.set_ylabel("Предсказанная цена ($)", fontsize=12)
-            ax.legend()
-            ax.grid(True)
-            st.pyplot(fig)
-            st.write('Точечный график, где по оси X — реальные цены, по оси Y — предсказанные. Идеальная модель даёт точки на красной линии (y = x). Исходя из графика, можно скачазать, что большинство всё было правильно.')
-    with tab2:
+
+
+    with tab3:
         st.subheader("Топ прогнозов по ошибке")
         results_df = pd.DataFrame({
             'Факт': y_test,
@@ -124,86 +226,7 @@ def main():
             alt.Y("count()", title="Количество домов")
         )
         st.altair_chart(chart, use_container_width=True)
-        st.write('График,показывающий, как часто встречаются дома в разных ценовых диапазонах. Пик в области $300–500k — большинство домов среднего класса.Длинный хвост справа — редкие дорогие объекты (выбросы). Вывод: Данные смещены в сторону недорогих домов, что может влиять на ошибки для luxury-сегмента.')
-
-    with tab3:
-        st.subheader("Зависимость цены от характеристик дома")
-        
-        col1, col2 = st.columns(2)
-        
-        with col1:
-            chart1 = alt.Chart(df).mark_circle().encode(
-                x=alt.X('bedrooms:O', axis=alt.Axis(title='Количество спален')),
-                y=alt.Y('price:Q', axis=alt.Axis(title='Цена')),
-                tooltip=['bedrooms', 'price']
-            ).properties(
-                width=400,
-                height=300,
-                title="Цена от количества спален"
-            ).interactive()
-            st.altair_chart(chart1, use_container_width=True)
-            st.write('Показывает, как количество комнат влияет на стоимость дома. Точки определяют каждый отдельный дом, самое популярное количество домов с 2-5 ванными.')
-            
-            chart3 = alt.Chart(df).mark_circle().encode(
-                x=alt.X('sqft_living:Q', axis=alt.Axis(title='Жилая площадь (кв. футы)')),
-                y=alt.Y('price:Q', axis=alt.Axis(title='Цена')),
-                tooltip=['sqft_living', 'price']
-            ).properties(
-                width=400,
-                height=300,
-                title="Цена от жилой площади"
-            ).interactive()
-            st.altair_chart(chart3, use_container_width=True)
-            st.write('Связь между жилой площадью участка (в квадратных футах) и ценой. Рост наблюдается на всём графике.')
-            
-            chart5 = alt.Chart(df).mark_circle().encode(
-                x=alt.X('view_scaled:Q', axis=alt.Axis(title='Оценка вида')),
-                y=alt.Y('price:Q', axis=alt.Axis(title='Цена')),
-                tooltip=['view_scaled', 'price']
-            ).properties(
-                width=400,
-                height=300,
-                title="Цена от оценки вида"
-            ).interactive()
-            st.altair_chart(chart5, use_container_width=True)
-            st.write('Зависимость цены от экспертной оценки вида из дома (шкала 0-1). Чем выше оценка, тем выше цена. Почти линейная зависимость.')
-        
-        with col2:
-            chart2 = alt.Chart(df).mark_circle().encode(
-                x=alt.X('bathrooms:O', axis=alt.Axis(title='Количество ванных комнат')),
-                y=alt.Y('price:Q', axis=alt.Axis(title='Цена')),
-                tooltip=['bathrooms', 'price']
-            ).properties(
-                width=400,
-                height=300,
-                title="Цена от количества ванных"
-            ).interactive()
-            st.altair_chart(chart2, use_container_width=True)
-            st.write('Показывает, как количество ванных комнат влияет на стоимость дома. Рост цены при увеличении ванных до определенного предела (3-3,5).')
-
-            chart4 = alt.Chart(df).mark_circle().encode(
-                x=alt.X('sqft_lot_scaled:Q', axis=alt.Axis(title='Площадь участка.')),
-                y=alt.Y('price:Q', axis=alt.Axis(title='Цена')),
-                tooltip=['sqft_lot_scaled', 'price']
-            ).properties(
-                width=400,
-                height=300,
-                title="Цена от площади участка"
-            ).interactive()
-            st.altair_chart(chart4, use_container_width=True)
-            st.write('Связь между площадью участка  и ценой. Резкий рост цены , затем слабая зависимость, имеются отдельные участки с большой площадью, скорее всего особняки, но из-за маленькой цены, видимо плохого качества.Площадь была стандартизирована')
-
-            chart6 = alt.Chart(df).mark_circle().encode(
-                x=alt.X('grade_scaled:Q', axis=alt.Axis(title='Оценка качества')),
-                y=alt.Y('price:Q', axis=alt.Axis(title='Цена')),
-                tooltip=['grade_scaled', 'price']
-            ).properties(
-                width=400,
-                height=300,
-                title="Цена от оценки качества"
-            ).interactive()
-            st.altair_chart(chart6, use_container_width=True)
-            st.write('Зависимость цены от экспертной оценки качества дома (шкала 0-1). Чем выше оценка, тем выше цена. Почти линейная зависимость.')
+        st.write('График,показывающий, как часто встречаются дома в разных ценовых диапазонах. Пик в области $300–500k — большинство домов среднего класса.Длинный хвост справа — редкие дорогие объекты (выбросы). Вывод: Данные смещены в сторону недорогих домов.')
 
         
             
@@ -219,11 +242,11 @@ def main():
         med_abs_proc_error = metric.get('Медианная абсолютная процентная ошибка', 0)
         MSE = metric.get('MSE', 0)
 
-        col1.metric("R² (Коэф. детерминации)", f"{r2_value:.3f}")
+        col1.metric("R² (Коэф. детерминации)", f"{r2_value:.2f}")
         col2.metric("MAE (Средняя ошибка)", f"{mae_value:,.0f}")
         col3.metric("Max Error", f"{max_error_value:,.0f}")
-        col4.metric("Медианная абсолютная процентная ошибка", f"%{med_abs_proc_error:,.3f}")
-        col5.metric("MSE", f"{MSE:,.3f}")
+        col4.metric("Медианная абсолютная процентная ошибка", f"{med_abs_proc_error:,.1f}%")
+        col5.metric("MSE", f"{MSE:,.0f}")
 
         st.subheader("Матрица корреляции признаков")
         corr = X_train.corr().stack().reset_index(name='correlation')
